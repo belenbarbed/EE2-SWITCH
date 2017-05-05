61dc74b321b9b87cddf17836f8092250aa07ab00
unknown <bb2115@eews104a-028.ic.ac.uk>
2017-05-05 10:27:08 +0100

    one threshold done

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 #include "mbed.h"
 #include "Adafruit_SSD1306.h"
 
-//Switch input definition
+// Switch input definition
 #define SW_PIN0 p21
 #define SW_PIN1 p22
 #define SW_PIN2 p23
 #define SW_PIN3 p24
 
-//Sampling period for the switch oscillator (us)
+// Sampling period for the switch oscillator (us)
 #define SW_PERIOD 20000 
 
 // Freq threshold for buttons
 #define THOLD_ON 70
 #define THOLD_OFF 74
 
-//Display interface pin definitions
+// Display interface pin definitions
 #define D_MOSI_PIN p5
 #define D_CLK_PIN p7
 #define D_DC_PIN p8
 #define D_RST_PIN p9
 #define D_CS_PIN p10
 
-//an SPI sub-class that sets up format and clock speed
+// an SPI sub-class that sets up format and clock speed
 class SPIPreInit : public SPI
 {
 public:
@@ -32,7 +32,7 @@ public:
     };
 };
 
-//Interrupt Service Routine prototypes (functions defined below)
+// Interrupt Service Routine prototypes (functions defined below)
 void sedge0();
 void sedge1();
 void sedge2();
@@ -40,19 +40,19 @@ void sedge3();
 
 void tout();
 
-//Output for the alive LED
+// Output for the alive LED
 DigitalOut alive(LED1);
 
-//External interrupt input from the switch oscillator
+// External interrupt input from the switch oscillator
 InterruptIn swin0(SW_PIN0);
 InterruptIn swin1(SW_PIN1);
 InterruptIn swin2(SW_PIN2);
 InterruptIn swin3(SW_PIN3);
 
-//Switch sampling timer
+// Switch sampling timer
 Ticker swtimer;
 
-//Registers for the switch counter, switch counter latch register and update flag
+// Registers for the switch counter, switch counter latch register and update flag
 volatile uint16_t scounter[4] = {0};
 volatile uint16_t scount[4] = {0};
 volatile uint16_t sfreq[4] = {0};
@@ -60,47 +60,52 @@ volatile bool son[4] = {0};
 
 volatile uint16_t update = 0;
 
-//Initialise SPI instance for communication with the display
+// Initialise SPI instance for communication with the display
 SPIPreInit gSpi(D_MOSI_PIN,NC,D_CLK_PIN); //MOSI,MISO,CLK
 
-//Initialise display driver instance
+// Initialise display driver instance
 Adafruit_SSD1306_Spi gOled1(gSpi,D_DC_PIN,D_RST_PIN,D_CS_PIN,64,128); //SPI,DC,RST,CS,Height,Width
 
 int main() { 
-    //Initialisation
+    // Initialisation
     gOled1.setRotation(0); //Set display rotation
     gOled1.clearDisplay();
     
-    //Attach switch oscillator counter ISR to the switch input instance for a rising edge
+    // Attach switch oscillator counter ISR to the switch input instance for a rising edge
     swin0.rise(&sedge0);
     swin1.rise(&sedge1);
     swin2.rise(&sedge2);
     swin3.rise(&sedge3);
     
-    //Attach switch sampling timer ISR to the timer instance with the required period
+    // Attach switch sampling timer ISR to the timer instance with the required period
     swtimer.attach_us(&tout, SW_PERIOD);
     
-    //Write some sample text
-    //gOled1.printf("%ux%u OLED Display\r\n", gOled1.width(), gOled1.height());
+    // Write some sample text
+    // gOled1.printf("%ux%u OLED Display\r\n", gOled1.width(), gOled1.height());
     
-    //Main loop
+    // Main loop
     while(1)
     {
-        //Has the update flag been set?       
+        // Has the update flag been set?       
         if (update) {
-            //Clear the update flag
+            // Clear the update flag
             update = 0;
             gOled1.setTextCursor(0,0);
             
             for(int i = 0; i < 4; i++) {
-                //Write the SW0 osciallor count as kHz
+                // Write the SW0 osciallor count as kHz
                 gOled1.printf("\nSW%01u: %02ukHz", i, sfreq[i]);
+                if(son[i]){
+                    gOled1.printf(" - ON ");
+                } else {
+                    gOled1.printf(" - OFF");
+                }
             }
             
-            //Copy the display buffer to the display
+            // Copy the display buffer to the display
             gOled1.display();
             
-            //Toggle the alive LED
+            // Toggle the alive LED
             alive = !alive;
         }
         
@@ -109,45 +114,45 @@ int main() {
 }
 
 
-//Interrupt Service Routine for rising edge on the switch oscillator input
+// Interrupt Service Routine for rising edge on the switch oscillator input
 void sedge0() {
-    //Increment the edge counter
+    // Increment the edge counter
     scounter[0]++;    
 }
 
 void sedge1() {
-    //Increment the edge counter
+    // Increment the edge counter
     scounter[1]++;    
 }
 
 void sedge2() {
-    //Increment the edge counter
+    // Increment the edge counter
     scounter[2]++;    
 }
 
 void sedge3() {
-    //Increment the edge counter
+    // Increment the edge counter
     scounter[3]++;    
 }
 
-//Interrupt Service Routine for the switch sampling timer
+// Interrupt Service Routine for the switch sampling timer
 void tout() {
     
     for(int i = 0; i < 4; i++) {
-        //Read the edge counter into the output register
+        // Read the edge counter into the output register
         scount[i] = scounter[i];
-        //Reset the edge counter
+        // Reset the edge counter
         scounter[i] = 0;
         // calculate the frequency
         sfreq[i] = (scount[i]*1000)/SW_PERIOD;
         // check if buttons are pressed
-        if((sfreq[i] < THOLD_ON) && (son[i]==0)) {
+        if(sfreq[i] < THOLD_ON) {
             son[i] = 1;
         } else {
             son[i] = 0;
         }
     }
     
-    //Trigger a display update in the main loop
+    // Trigger a display update in the main loop
     update = 1;
 }
